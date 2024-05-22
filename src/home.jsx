f565d076e60254cb9d412698330d7038431cb72c
RamyaMidde <sathyasoftechin@gmail.com>
2024-05-22 16:30:21 +0530

    changes

---
+++ src/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div 
+            style={{ 
+                color: '#0dcaf0', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                height: '100vh', 
+                textAlign: 'center' 
+            }} 
+        >
+            <h1>We are Coming Soon....</h1>
+        </div>
+    );
+}
+
+export default Home;
